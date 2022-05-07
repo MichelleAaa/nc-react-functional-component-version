b80efda2bbe2fb705c75b2592b69928204fa4e32
Michelle A <michelleameral@gmail.com>
2022-05-06 18:02:03 -0700

    Breadcrumbs and Comments Components

--- src/pages/CampsitesDirectoryPage.js
+++ src/pages/CampsitesDirectoryPage.js
@@ -1,10 +1,11 @@
-import { useState } from 'react';
+// import { useState } from 'react';
 import { Container, Row, Col, Button } from 'reactstrap';
-import CampsiteDetail from '../features/campsites/CampsiteDetail';
+// import CampsiteDetail from '../features/campsites/CampsiteDetail';
 import CampsitesList from '../features/campsites/CampsitesList';
-import { selectCampsiteById  } from '../features/campsites/campsitesSlice';
+import SubHeader from '../components/SubHeader';
+// import { selectCampsiteById  } from '../features/campsites/campsitesSlice';
 
-const CampsitesDirectoryPage = () => {
+// const CampsitesDirectoryPage = () => {
     // let selectedCampsite = selectRandomCampsite();
 
     // const toggleCampsite = () => {
@@ -14,24 +15,33 @@ const CampsitesDirectoryPage = () => {
 
     // const [selectedCampsite, toggleCampsite ] = useState(selectRandomCampsite());
 
-    const [campsiteId, setCampsiteId] = useState(0);
-    const selectedCampsite = selectCampsiteById(campsiteId);
+    // const [campsiteId, setCampsiteId] = useState(0);
+    // const selectedCampsite = selectCampsiteById(campsiteId);
 
-    return (
-        <Container>
+    // return (
+    //     <Container>
             {/* <Button onClick={() => toggleCampsite()}> */}
             {/* <Button onClick={() => toggleCampsite(selectRandomCampsite())}>
                 Select Random Campsite
             </Button> */}
-            <Row>
-                <Col sm='5' md='7'>
-                    {/* <CampsitesList /> */}
-                    <CampsitesList setCampsiteId={setCampsiteId} />
-                </Col>
-                <Col sm='7' md='5'>
-                    <CampsiteDetail campsite={selectedCampsite} />
-                </Col>
-            </Row>
+//             <Row>
+//                 <Col sm='5' md='7'>
+//                     {/* <CampsitesList /> */}
+//                     <CampsitesList setCampsiteId={setCampsiteId} />
+//                 </Col>
+//                 <Col sm='7' md='5'>
+//                     <CampsiteDetail campsite={selectedCampsite} />
+//                 </Col>
+//             </Row>
+//         </Container>
+//     );
+// };
+
+const CampsitesDirectoryPage = () => {
+    return (
+        <Container>
+            <SubHeader current='Directory' />
+            <CampsitesList />
         </Container>
     );
 };
